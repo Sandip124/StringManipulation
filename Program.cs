31778ac2cbaa48fdc61389b0b650eac13e5e300f
Sandip124 <chaudharysandip124@gmail.com>
2019-01-29 16:41:05 +0545

    Function upgraded

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 
 namespace StringManipulation
@@ -10,36 +11,94 @@ namespace StringManipulation
     {
         static void Main(string[] args)
         {
-
             string path = @"D:\Dotnet Projects\StringManipulation\cdn-list.txt";
             string prefix = "www";
             List<string> domains = DomainExtractor(path, prefix);
+            CreateFileFromList(domains, "domainLists.txt");
 
-            foreach (var domain in domains)
-            {
-                System.Console.WriteLine(domain);
-            }
+            string url = "https://stackexchange.com/sites?view=list#name";
+            string urlData = CreateStringFromPostRequest(url);
+            CreateFileFromString(urlData, "urldata.html");
         }
 
         public static List<string> DomainExtractor(string Path, string prefix = "Prefix")
         {
-            string[] lines = System.IO.File.ReadAllLines(@Path);
-            List<string> sb = new List<string>();
-            foreach (string line in lines)
+            try
             {
-                string[] data = line.Split(',');
-                string[] domain_parts = data[0].Trim().Split('"');
-                string domain = domain_parts[1];
-                if (domain.IndexOf('.') == 0)
+                string[] lines = System.IO.File.ReadAllLines(@Path);
+                List<string> list = new List<string>();
+                foreach (string line in lines)
                 {
-                    sb.Add(prefix + domain);
+                    string[] data = line.Split(',');
+                    string[] domain_parts = data[0].Trim().Split('"');
+                    string domain = domain_parts[1];
+                    if (domain.IndexOf('.') == 0)
+                    {
+                        list.Add(prefix + domain);
+                    }
+                    else
+                    {
+                        list.Add(prefix + '.' + domain);
+                    }
                 }
-                else
+                return list;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error occured:" + e);
+            }
+        }
+
+        public static void CreateFileFromList(List<string> data, string path)
+        {
+            try
+            {
+                if (!File.Exists(path))
                 {
-                    sb.Add(prefix + '.' + domain);
+                    using (var file = new StreamWriter(@path))
+                    {
+                        data.ForEach(v => file.WriteLine(v));
+                    }
                 }
             }
-            return sb;
+            catch (Exception e)
+            {
+                throw new Exception("Error occured:" + e);
+            }
+        }
+        public static void CreateFileFromString(string data, string path)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@path, true))
+                    {
+                        file.Write(data);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error occured:" + e);
+            }
+
+        }
+
+        public static string CreateStringFromPostRequest(string url)
+        {
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(@url);
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                return responseString;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error occured:" + e);
+            }
+
         }
     }
 }
