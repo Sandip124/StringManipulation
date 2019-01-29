fc28928031cc53f8f0d56cce7c694a136f9e591e
Sandip124 <chaudharysandip124@gmail.com>
2019-01-29 13:28:52 +0545

    Using List to  manipulate  strings

--- Program.cs
+++ Program.cs
@@ -1,37 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Text;
 
 namespace StringManipulation
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
 
-            string path =@"D:\Dotnet Projects\StringManipulation\cdn-list.txt";
-            string prefix = "WWW";
-            StringBuilder domains = DomainExtractor(path,prefix);
-            Console.WriteLine(domains);
+            string path = @"D:\Dotnet Projects\StringManipulation\cdn-list.txt";
+            string prefix = "www";
+            List<string> domains = DomainExtractor(path, prefix);
+
+            foreach (var domain in domains)
+            {
+                System.Console.WriteLine(domain);
+            }
         }
 
-        public static StringBuilder DomainExtractor(string Path,string prefix = "Prefix")
+        public static List<string> DomainExtractor(string Path, string prefix = "Prefix")
         {
             string[] lines = System.IO.File.ReadAllLines(@Path);
-            StringBuilder sb = new StringBuilder();
+            List<string> sb = new List<string>();
             foreach (string line in lines)
             {
                 string[] data = line.Split(',');
-                string [] domain_parts = data[0].Trim().Split('"');
+                string[] domain_parts = data[0].Trim().Split('"');
                 string domain = domain_parts[1];
                 if (domain.IndexOf('.') == 0)
                 {
-                    sb.Append(prefix + domain);
+                    sb.Add(prefix + domain);
                 }
                 else
                 {
-                    sb.Append(prefix +'.'+ domain);
+                    sb.Add(prefix + '.' + domain);
                 }
-                sb.Append("\n");
             }
             return sb;
         }
